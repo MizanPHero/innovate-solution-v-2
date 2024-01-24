4bc4c013e3e6559494f8c0f998aadc07894d1670
Mizanur Rahman <mizanrxb@gmail.com>
2024-01-24 17:48:35 +0600

    reserving for loadshedding

--- src/components/SearchFieldTop.jsx
+++ src/components/SearchFieldTop.jsx
@@ -21,17 +21,58 @@ const SearchFieldTop = () => {
     const [selectedOption, setSelectedOption] = useState('flight');
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+
+    //from destination
+    const [fromSelectedOption, setFromSelectedOption] = useState(null);
+    const [isFromOpen, setIsFromOpen] = useState(false);
+    const handleFromToggle = () => {
+    setIsFromOpen(!isFromOpen);
+    };
+    const handleFromSelect = (name, airport) => {
+    const selectedOption = {
+        name: name,
+        airport: airport,
+    };
+    setFromSelectedOption(selectedOption);
+    setIsFromOpen(false);
+    };
+
+    //to destination
+    const [toSelectedOption, setToSelectedOption] = useState(null);
+    const [isToOpen, setIsToOpen] = useState(false);
+    const handleToToggle = () => {
+    setIsToOpen(!isToOpen);
+    };
+    const handleToSelect = (name, airport) => {
+    const selectedOption = {
+        name: name,
+        airport: airport,
+    };
+    setToSelectedOption(selectedOption);
+    // logic or callback if needed
+    setIsToOpen(false); 
+    };
+
+
+    //destination value exchange
+    const handleDestinationExchange = () => {
+        const tempFromOption = { ...fromSelectedOption };
+        setFromSelectedOption({ ...toSelectedOption });
+        setToSelectedOption(tempFromOption);
+      };
+
+
+
     // calendar
     const days = ["S", "M", "T", "W", "T", "F", "S"];
 	const currentDate = dayjs();
 	const [today, setToday] = useState(currentDate);
 	const [selectDate, setSelectDate] = useState(currentDate);
-
     const handleOptionChange = (event) => {
         setSelectedOption(event.target.value);
     };
 
-
+    // for calendar
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
       };
@@ -39,7 +80,8 @@ const SearchFieldTop = () => {
       const closeMenu = () => {
         setIsMenuOpen(false);
       };
-
+      
+    //for radio
     const options = [
         { id: "hotel", value: "hotel", label: "Hotel" },
         { id: "flight", value: "flight", label: "Flight" },
@@ -65,23 +107,15 @@ const SearchFieldTop = () => {
     
     
     const dateString = selectDate.toDate().toDateString();
-    
-  
     const dateParts = dateString.split(' ');
-    
-   
     const abbreviatedDayOfWeek = dateParts[0]; 
     const formattedDate = dateParts.slice(1).join(' '); 
-    
-    
     const fullDayOfWeek = getFullDayName(abbreviatedDayOfWeek);
-    
-    
     const formattedDateWithFullDay = `${selectDate.format('D')} ${selectDate.format('MMMM YYYY')}`;
     
-    
-    console.log(fullDayOfWeek); // Output: "Saturday"
-    console.log(formattedDateWithFullDay); // Output: "27 January 2024"
+
+    console.log(fromSelectedOption);
+    console.log(toSelectedOption);
 
 
 
@@ -127,7 +161,7 @@ const SearchFieldTop = () => {
                 {/* second line elements */}
                 <div className="flex flex-col sm:flex-row gap-4 items-center sm:gap-[18px] justify-between w-full mt-6">
                     {/* left side tabs */}
-                    <div className="flex flex-col w-full sm:w-auto items-center sm:items-start gap-2 pl-[14px] pr-[14px]  sm:pr-[2px] py-[8px] rounded-2xl  bg-primaryBlue">
+                    <div className="flex flex-col w-full sm:w-[125px] items-center sm:items-start gap-2 pl-[14px] pr-[14px]  sm:pr-[2px] py-[8px] rounded-2xl  bg-primaryBlue">
                         <button className="flex items-center justify-center gap-2">
                             <OneWay/>
                             <span className="text-sm font-normal text-[#fff]">One-Way</span>
@@ -143,38 +177,137 @@ const SearchFieldTop = () => {
                     </div>
 
                     {/* from and to element */}
-                    <div className="flex">
-                        <div className="flex border rounded-2xl border-fadeGray">
+                    <div className="flex w-auto sm:w-[463px]">
+                        <div className="relative flex justify-between w-full border rounded-2xl border-fadeGray">
                             {/* First Child Div */}
-                            <div className="my-4 ml-4 mr-3 sm:my-3 sm:ml-5 sm:mr-11 ">
+                            {/* From */}
+                            {/* <div className="my-4 ml-4 mr-3 sm:my-3 sm:ml-5 sm:mr-11 ">
                                 <div>
                                     <p className="font-normal text-[15px] text-lightDark">From</p>
                                     <p className="text-lg font-medium text-deepDark">Jakarta</p>
                                     <p className="font-normal text-[15px] text-lightDark">Soekarno-Hatta Airport</p>
                                 </div>
-                            </div>
+                            </div> */}
+                                
+                            <button
+                                type="button"
+                                className="w-full my-4 ml-4 mr-3 sm:my-3 sm:ml-7 sm:mr-7"
+                                id="menu-button"
+                                aria-expanded={isFromOpen}
+                                aria-haspopup="true"
+                                onClick={handleFromToggle}
+                            >
+                                <div className="text-left">
+                                    <p className="font-normal text-[15px] text-lightDark">From</p>
+                                    <p className="text-lg font-medium text-deepDark">{fromSelectedOption?.name ? fromSelectedOption?.name : "Not Selected"}</p>
+                                    <p className="font-normal text-[15px] text-lightDark">
+                                        {fromSelectedOption?.airport
+                                            ? fromSelectedOption.airport.length > 21
+                                                ? `${fromSelectedOption.airport.slice(0, 21)}...`
+                                                : fromSelectedOption.airport
+                                            : "Select your destination"}
+                                    </p>
+                                </div>
+                            </button>
+
+                            {isFromOpen && (
+                                <div
+                                    className="absolute left-0 top-24 mt-2 z-50 origin-top-right bg-[#fff] rounded-md shadow-lg w-fit ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                    role="menu"
+                                    aria-orientation="vertical"
+                                    aria-labelledby="menu-button"
+                                    tabIndex="-1"
+                                >
+                                    <div className="py-1" role="none">
+                                        <div
+                                            className="px-4 py-2 rounded-md hover:bg-fadeGray hover:bg-opacity-70"
+                                            onClick={() => handleFromSelect("Jakarta", "Soekarno-Hatta Airport")}
+                                        >
+                                            <h2 className="text-base font-medium text-deepDark">Jakarta</h2>
+                                            <p className="text-sm font-normal text-lightDark">Soekarno-Hatta Airport</p>
+                                        </div>
+                                        <div
+                                            className="px-4 py-2 rounded-md hover:bg-fadeGray hover:bg-opacity-40"
+                                            onClick={() => handleFromSelect("Paris", "Charles de Gaulle Airport")}
+                                        >
+                                            <h2 className="text-base font-medium text-deepDark">Paris</h2>
+                                            <p className="text-sm font-normal text-lightDark">Charles de Gaulle Airport</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
+
+
 
                             {/* Separator */}
                             <div className="relative mx-2 border-r border-fadeGray">
-                                <button className="absolute hidden sm:flex border border-fadeGray top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 items-center justify-center w-10 h-10 rounded-full bg-[#fff]">
+                                <button className="absolute hidden hover:bg-fadeGray sm:flex border border-fadeGray top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 items-center justify-center w-10 h-10 rounded-full bg-[#fff]" onClick={handleDestinationExchange}>
                                     <img src={switchIcon}/>
                                 </button>
                             </div>
 
                             {/* Second Child Div */}
-                            <div className="my-4 ml-3 mr-2 sm:ml-12 sm:mr-7 ">
+                            {/* <div className="my-4 ml-3 mr-2 sm:ml-12 sm:mr-7 ">
                                 <div>
                                     <p className="font-normal text-[15px] text-lightDark">To</p>
                                     <p className="text-lg font-medium text-deepDark">Jakarta</p>
                                     <p className="font-normal text-[15px] text-lightDark">Soekarno-Hatta Airport</p>
                                 </div>
-                            </div>
+                            </div> */}
+
+                            <button
+                                type="button"
+                                className="w-full my-4 ml-3 mr-2 sm:ml-7 sm:mr-7"
+                                id="menu-button"
+                                aria-expanded={isToOpen}
+                                aria-haspopup="true"
+                                onClick={handleToToggle}
+                            >
+                                <div className="text-left">
+                                    <p className="font-normal text-[15px] text-lightDark">To</p>
+                                    <p className="text-lg font-medium text-deepDark">{toSelectedOption?.name ? toSelectedOption?.name : "Not Selected"}</p>
+                                    <p className="font-normal text-[15px] text-lightDark">{toSelectedOption?.airport
+                                        ? toSelectedOption.airport.length > 21
+                                            ? `${toSelectedOption.airport.slice(0, 21)}...`
+                                            : toSelectedOption.airport
+                                        : "Select your destination"}</p>
+                                </div>
+                            </button>
+
+                            {isToOpen && (
+                                <div
+                                    className="absolute right-0 top-24 z-50 origin-top-right bg-[#fff] rounded-md shadow-lg w-fit ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                    role="menu"
+                                    aria-orientation="vertical"
+                                    aria-labelledby="menu-button"
+                                    tabIndex="-1"
+                                >
+                                    <div className="py-1" role="none">
+                                    <div
+                                        className="px-4 py-2 rounded-md hover:bg-fadeGray hover:bg-opacity-70"
+                                        onClick={() => handleToSelect("Jakarta", "Soekarno-Hatta Airport")}
+                                    >
+                                        <h2 className="text-base font-medium text-deepDark">Jakarta</h2>
+                                        <p className="text-sm font-normal text-lightDark">Soekarno-Hatta Airport</p>
+                                    </div>
+                                    <div
+                                        className="px-4 py-2 rounded-md hover:bg-fadeGray hover:bg-opacity-40"
+                                        onClick={() => handleToSelect("Paris", "Charles de Gaulle Airport")}
+                                    >
+                                        <h2 className="text-base font-medium text-deepDark">Paris</h2>
+                                        <p className="text-sm font-normal text-lightDark">Charles de Gaulle Airport</p>
+                                    </div>
+                                    </div>
+                                </div>
+                                )}
+
+
                         </div>
                     </div>
 
                     {/* depart and return element */}
-                    <div className="flex">
-                        <div className="relative flex border rounded-2xl border-fadeGray">
+                    <div className="flex w-auto sm:w-[351px]">
+                        <div className="relative flex w-full border rounded-2xl border-fadeGray">
                             {/* First Child Div */}
                             {/* Depart element */}
                             <button className="flex flex-col items-center mx-4 my-3" id="menu-button"
@@ -293,9 +426,8 @@ const SearchFieldTop = () => {
                     </div>
 
 
-                    {/* passenger element */}
 
-                    <div className="flex w-full sm:w-auto">
+                    <div className="flex w-full sm:w-[177px]">
                         <div className="flex w-full border rounded-2xl border-fadeGray">
                             <div className="flex flex-col items-center m-4 mr-14">
                                 <div>
